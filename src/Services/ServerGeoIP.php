8569f91c7667abaa6d6e4819cb1fca83cfc3edcd
Denis Davidov <denis.davidov@instafxgroup.com>
2021-01-27 18:56:12 +0200

    ignore del vendor base class

---
+++ src/Services/ServerGeoIP.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @author Denis Davydov
+ * @copyright © 27.01.2021, InstaForex Group
+ * Date: 27.01.2021
+ */
+
+namespace LaravelGeo\Services;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Config;
+
+class ServerGeoIP implements IGeoIP
+{
+    private $_params_key;
+    private $_headers;
+
+    public function __construct(Request $request)
+    {
+        $this->_params_key = Config::get('geo.key_params');
+        $this->_headers = collect($request->headers->all())->transform(function($item) {
+            return $item[0];
+        });
+    }
+
+    public function get(): array
+    {
+        return array_intersect_key($this->_headers->all(), array_flip(array_keys($this->_params_key)));
+    }
+}
